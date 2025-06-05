ebf943f6f865808222250bb96008ae01a328f378
Adrian Dwiputra <e0915700@nus.edu.sg>
2025-06-05 15:16:38 +0800

    hexBin plot for figure 2 as suggested by ZY

--- scripts/plots/figure2.R
+++ scripts/plots/figure2.R
@@ -9,7 +9,8 @@ library(viridis)
 # 1. INPUTS=====
 inputDir <- "D:/Documents/research/projects/nus07_fire/analysis/output/"
 # inputDir_vector <- "D:/Documents/research/projects/nus07_fire/analysis/finalized_materials/"
-inputDir_vector <- "F:/temp_donotDelete/finalized_materials/"
+# inputDir_vector <- "F:/temp_donotDelete/finalized_materials/"
+inputDir_vector <- "E:/temp_transfer/finalized_materials/"
 ignitionSpread_pointSummary <- fread(paste0(inputDir, "ignitionSpread_summary.csv"))
 convexHull_polygon <- vect(paste0(inputDir_vector, "convexHull_final2015fireNetwork.shp")) %>% values()
 
@@ -32,13 +33,13 @@ ignitionArea_summary <- convexHull_polygon %>% rename(graph_id = id) %>% left_jo
 # Diversity", "Distance to Water"))
 
 # 3. PLOTTING=========
-# a. Scatter plot for ignition / spread with loess line adopted with modification from: https://ademos.people.uic.edu/Chapter10.html
-scatter_ignitionSpread = ggplot(data = ignitionSpread_pointSummary, aes(x = n_ignitions, y = n_spread))+
+# a. Scatter plot for ignition / spread
+scatter_ignitionSpread = ggplot(data = ignitionSpread_pointSummary, aes(x = log(n_ignitions), y = log(n_spread))) +
   geom_point(alpha = 0.08)+
   # geom_smooth(color='black')+
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+  
-  xlab("Number of origin points")+ylab("Number of spread points") #+
+  xlab("log10(Number of origin points)")+ylab("log10(Number of spread points)") #+
   # scale_x_continuous(breaks = seq(0, 50, 5))+
   # scale_y_continuous(breaks = seq(50, 90, 5))
 # pointDensity_ignitionSpread = ggplot(data = ignitionSpread_pointSummary, aes(x = n_ignitions, y = n_spread))+
@@ -47,10 +48,56 @@ scatter_ignitionSpread = ggplot(data = ignitionSpread_pointSummary, aes(x = n_ig
 #   theme_bw() +
 #   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+  
 #   xlab("Number of origin points")+ylab("Number of spread points") #+
-# b. Scatter plot for ignition / convexHull area with loess line adopted with modification from: https://ademos.people.uic.edu/Chapter10.html
-scatter_ignitionConvHull = ggplot(data = ignitionArea_summary, aes(x = n_ignitions, y = km_area))+
-  geom_point(alpha = 0.08)+
+# b. Scatter plot for ignition / convexHull area
+scatter_ignitionConvHull = ggplot(data = ignitionArea_summary, aes(x = log(n_ignitions), y = log(km_area)))+
+  geom_point(alpha = 0.08)
   # geom_smooth(color='black')+
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+  
-  xlab("Number of origin points")+ylab("Area of convex hull (square km)") 
+  xlab("log10(Number of origin points)")+ylab("log10(km Area of convex hull)")
+
+# c. Hexbin for ignition / spread
+png(file = paste0(inputDir, "plots/figure2a_hexBin.png"), width=1400, height = 1300, units = "px")
+hexBin_ignitionSpread = ggplot(data = ignitionSpread_pointSummary, aes(x = log(n_ignitions), y = log(n_spread))) +
+  geom_hex(bins = 15) + scale_fill_viridis() +
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+  
+  xlab("log10(Number of origin points)")+ylab("log10(Number of spread points)")+
+  labs(fill = "Count")+
+  theme(axis.title=element_text(size=50), axis.text=element_text(size=30),
+        legend.position = c(.95, 0.1),
+        legend.justification = c("right", "bottom"),
+        legend.box.just = "right",
+        legend.margin = margin(6, 6, 6, 6),
+        legend.text = element_text(size = 28),
+        legend.title = element_text(size = 30),
+        legend.key.size = unit(1.25, "cm")
+  )
+hexBin_ignitionSpread
+#+
+dev.off()
+# d. Hexbin for ignition / spread
+png(file = paste0(inputDir, "plots/figure2b_hexBin.png"), width=1400, height = 1300, units = "px")
+hexBin_ignitionConvHull = ggplot(data = ignitionArea_summary, aes(x = log(n_ignitions), y = log(km_area))) +
+  geom_hex(bins = 15) + scale_fill_viridis() +
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+  
+  xlab("log10(Number of origin points)")+ylab("log10(km Area of convex hull)")+
+  labs(fill = "Count")+
+  theme(axis.title=element_text(size=50), axis.text=element_text(size=30),
+        legend.position = c(.95, 0.1),
+        legend.justification = c("right", "bottom"),
+        legend.box.just = "right",
+        legend.margin = margin(6, 6, 6, 6),
+        legend.text = element_text(size = 28),
+        legend.title = element_text(size = 30),
+        legend.key.size = unit(1.25, "cm")
+  )
+hexBin_ignitionConvHull
+dev.off()
+# Linear model of ignition points vs. conv hull
+# lm_convHull <- lm(log(km_area) ~ sqrt(n_ignitions), data = ignitionArea_summary)
+# summary(lm_convHull)
+# 
+# ignitionArea_summary <- ignitionArea_summary %>% mutate(expVariable = n_ignitions^(1/4))
+# lm_convHull <- lm(log(km_area) ~ sqrt(n_ignitions), data = ignitionArea_summary)
