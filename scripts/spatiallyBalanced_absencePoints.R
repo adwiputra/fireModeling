8958e298e85dbdc7e8cb0beb30a486ccd8019369
Adrian Dwiputra <e0915700@nus.edu.sg>
2025-06-11 16:56:28 +0800

    adjusting the absence point production for the rerun.

--- scripts/spatiallyBalanced_absencePoints.R
+++ scripts/spatiallyBalanced_absencePoints.R
@@ -8,12 +8,15 @@ library(spbal)
 library(sf)
 library(ggplot2)
 library(gridExtra)
+library(tidyverse)
 
 # 1. INPUTS=======
-output_dir <- "D:/Documents/research/projects/nus07_fire/analysis/finalized_materials/"
-shp_studySite <- st_read("D:/Documents/research/projects/nus07_fire/analysis/finalized_materials/absence_spatialBound_intersect_dissolve_4km.shp")
+output_dir <- "E:/temp_transfer/finalized_materials/basSampled_absences/"
+if(!dir.exists(output_dir))dir.create(output_dir)
+shp_studySite <- st_read(paste0(dirname(output_dir), "/absence_spatialBound_intersect_dissolve_4km.shp"))
 bb <- BoundingBox(shapefile = shp_studySite)
-n_samples <- 75424 # matches the number of ignition points
+n_samples <- read.csv(paste0(dirname(output_dir), "/ignitionSpread_machLearn_input.csv")) %>% filter(pointCategory == "ignition") %>%
+  select(LATITUDE, LONGITUDE) %>% unique() %>% nrow()#set to 75424 previously # matches the number of ignition points
 
 # 2. PROCESSING=========
 seeds <- c(411, 4, 11, 1, 5, 8, 20, 41, 21, 19)
