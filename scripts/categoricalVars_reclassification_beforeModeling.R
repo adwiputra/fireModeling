44d99998f29dbf47f250174d7a1e03e06b6fefb9
Adrian Dwiputra <e0915700@nus.edu.sg>
2025-06-17 13:19:47 +0800

    reclassified categorical variables into a factor friendly format.

--- scripts/categoricalVars_reclassification_beforeModeling.R
+++ scripts/categoricalVars_reclassification_beforeModeling.R
@@ -9,17 +9,16 @@ library(terra)
 library(biomod2)
 library(data.table)
 
-
+# a. Version 2-----
 # 2. INPUTS=====
-landCoverDir <- "F:/temp_donotDelete/data/spatial/covariates/landCover_CCI/"
-categoricalVariables <- c("gridcode", "reduced_ecoregion_raster")
-landCover_raster <- "F:/temp_donotDelete/finalized_materials/na_synced_covariates_v2/dominantLandCover.tif" %>% rast() %>% as.factor()
+landCoverDir <- "F:/storage/projects/nus07_fire/analysis/data/spatial/covariates/landCover_CCI/"
+landCover_raster <- "F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/dominantLandCover.tif" %>% rast() %>% as.factor()
 landCover_freq <- landCover_raster %>% freq()
 # Ecoregion raster attribute table
-ecoregion_raster <- "F:/temp_donotDelete/finalized_materials/na_synced_covariates_v2/Ecoregion.tif" %>% rast() %>% as.factor()
+ecoregion_raster <- "F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/Ecoregion.tif" %>% rast() %>% as.factor()
 ecoregion_freq <- ecoregion_raster %>% freq()
 # Reduced ecoregion vector layer to extract the attribute table
-ecoregion_lookup <- "F:/temp_donotDelete/finalized_materials/reduced_ecoregion.shp" %>% vect() %>% values()
+ecoregion_lookup <- "F:/storage/projects/nus07_fire/analysis/finalized_materials/reduced_ecoregion.shp" %>% vect() %>% values()
 
 # 3. PREPROCESSING======
 # Prepping landCover_lookup
@@ -31,22 +30,48 @@ landCover_freq <- landCover_freq %>% mutate(value = as.factor(value)) %>% rename
 # joining landCover_freq and landCover_lookup
 landCover_lookup <- landCover_lookup %>% mutate(Value = as.factor(Value)) %>% right_join(landCover_freq)
 # Save landCover_lookup as reference for interpretation later
-write.csv(landCover_lookup, "F:/temp_donotDelete/finalized_materials/landCover_lookup.csv", row.names = FALSE)
+write.csv(landCover_lookup, "F:/storage/projects/nus07_fire/analysis/finalized_materials/landCover_lookup.csv", row.names = FALSE)
 #ADhere
 # Cleaning ecoreg_lookup
 ecoregion_lookup <- ecoregion_lookup %>% select(newECONAME, rasterVal) %>% rename(value = rasterVal)
 # join with the freq table
 ecoregion_lookup <- ecoregion_freq %>% mutate(value = as.numeric(value)) %>% left_join(ecoregion_lookup) %>% mutate(expl.val = as.numeric(as.factor(value)))
 # Save landCover_lookup as reference for interpretation later
-write.csv(ecoregion_lookup, "F:/temp_donotDelete/finalized_materials/ecoregion_lookup.csv", row.names = FALSE)
+write.csv(ecoregion_lookup, "F:/storage/projects/nus07_fire/analysis/finalized_materials/ecoregion_lookup.csv", row.names = FALSE)
 # 4. PROCESSING======
 # reclassify land cover raster to match the expl.val as specified in landCover_lookup
 reclassification_matrix <- landCover_lookup %>% select(Value, expl.val) %>% mutate(Value = as.numeric(as.character(Value))) %>% mutate(expl.val = as.numeric(expl.val)) %>% as.matrix()
 landCover_raster <- landCover_raster %>% terra::classify(reclassification_matrix)
 # Export raster
-writeRaster(landCover_raster, "F:/temp_donotDelete/finalized_materials/na_synced_covariates_v2/dominantLandCover_v2.tif")
+writeRaster(landCover_raster, "F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/dominantLandCover_v2.tif")
 # reclassify ecoregion raster to match the expl.val as specified in ecoregion_lookup
 reclassification_matrix <- ecoregion_lookup %>% select(value, expl.val) %>% as.matrix()
 ecoregion_raster <- ecoregion_raster %>% terra::classify(reclassification_matrix)
 # Export raster
-writeRaster(ecoregion_raster, "F:/temp_donotDelete/finalized_materials/na_synced_covariates_v2/Ecoregion_v2.tif")
+writeRaster(ecoregion_raster, "F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/Ecoregion_v2.tif")
+
+# b. Version 3=====
+# INPUTS=====
+# landCover_raster v2
+landCover_raster <- rast("F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/dominantLandCover_v2.tif")
+# landCover_lookup v2
+landCover_lookup <- read.csv("F:/storage/projects/nus07_fire/analysis/finalized_materials/landCover_lookup.csv") %>%
+  mutate(v3.val = expl.val-1+10)
+# ecoregion_raster v2
+ecoregion_raster <- rast("F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/Ecoregion_v2.tif")
+# ecoregion_lookup v2
+ecoregion_lookup <- read.csv("F:/storage/projects/nus07_fire/analysis/finalized_materials/ecoregion_lookup.csv") %>%
+  mutate(v3.val = expl.val-1+10)
+# reclassify landCover raster to match the v3.val as specified in landCover_lookup.
+# at this point, interpretation should be based on the expl.val which is expected
+# to be the factor level.
+reclassification_matrix <- landCover_lookup %>% select(expl.val, v3.val) %>% as.matrix()
+landCover_raster <- landCover_raster %>% terra::classify(reclassification_matrix) %>% terra::as.factor()
+# Export raster
+writeRaster(landCover_raster, "F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/dominantLandCover_v3.tif")
+
+# Repeat the same steps for ecoregion_raster
+reclassification_matrix <- ecoregion_lookup %>% select(expl.val, v3.val) %>% as.matrix()
+ecoregion_raster <- ecoregion_raster %>% terra::classify(reclassification_matrix) %>% terra::as.factor()
+# Export raster
+writeRaster(ecoregion_raster, "F:/storage/projects/nus07_fire/analysis/finalized_materials/na_synced_covariates_v2/Ecoregion_v3.tif")
